a2f8eb4a43dbd81b77ca195ee6fb6664d2daa535
Gordon P Reilley Jr <gordie378@gmail.com>
2022-12-28 11:53:24 -0500

    almost done with CC06

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -28,6 +28,37 @@ class LinkedList:
             current = current.next
         return string + "NULL"
 
+    def append(self, value):
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(value)
+
+    def insert_before(self, before, value):
+        current = self.head
+        previous = None
+        try:
+            while current.value is not before:
+                previous = current
+                current = current.next
+            if previous is not None:
+                previous.next = Node(value, current)
+            if previous is None:
+                self.head = Node(value, current)
+        except Exception as e:
+            raise TargetError(e)
+
+    def insert_after(self, after, value):
+        current = self.head
+        try:
+            while current.value is not after:
+                current = current.next
+            new_node = Node(value)
+            new_node.next = current.next
+            current.next = new_node
+        except Exception as e:
+            raise TargetError(e)
+
 
 class TargetError:
     pass
